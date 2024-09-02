cfdc950210abea39b89ed412b18248d0f1efadaa
masiham <masihmooohmmadpoor@gmail.com>
2024-09-02 14:59:37 +0330

    created User Activity section

--- src/app/(dashboard)/dashboard/data/page.jsx
+++ src/app/(dashboard)/dashboard/data/page.jsx
@@ -1,8 +1,11 @@
+"use client"
 import { IoHome } from "react-icons/io5";
 import { IoIosArrowForward } from "react-icons/io";
 import {
+  Badge,
   Button,
   Checkbox,
+  Fab,
   FormControlLabel,
   FormGroup,
   IconButton,
@@ -11,9 +14,72 @@ import {
 } from "@mui/material";
 import { FormLabel } from "@mui/material";
 import { FiPlus } from "react-icons/fi";
-import Avatar from '@mui/material/Avatar';
+import Avatar from "@mui/material/Avatar";
+import AddIcon from "@mui/icons-material/Add";
+import AccessTimeTwoToneIcon from "@mui/icons-material/AccessTimeTwoTone";
+import { styled } from '@mui/material/styles';
 
 const Data = () => {
+
+  const StyledBadgeSuccess = styled(Badge)(({ theme }) => ({
+    "& .MuiBadge-badge": {
+      backgroundColor : "#22c55e",
+      color : "#22c55e",
+      boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+      "&::after": {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        borderRadius: "50%",
+        animation: "ripple 1.2s infinite ease-in-out",
+        border: "1px solid currentColor",
+        content: '""',
+      },
+    },
+  }));
+
+  const StyledBadgeError = styled(Badge)(({ theme }) => ({
+    "& .MuiBadge-badge": {
+      backgroundColor : "#f43f5e",
+      color : "#f43f5e",
+      boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+      "&::after": {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        borderRadius: "50%",
+        animation: "ripple 1.2s infinite ease-in-out",
+        border: "1px solid currentColor",
+        content: '""',
+      },
+    },
+  }));
+
+  const StyledBadgeWarning = styled(Badge)(({ theme }) => ({
+    "& .MuiBadge-badge": {
+      backgroundColor : "#eab308",
+      color : "#eab308",
+      boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+      "&::after": {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        borderRadius: "50%",
+        animation: "ripple 1.2s infinite ease-in-out",
+        border: "1px solid currentColor",
+        content: '""',
+      },
+    },
+  }));
+
+
+
   return (
     <section className="flex flex-col gap-y-6">
       {/* 1 */}
@@ -36,7 +102,7 @@ const Data = () => {
           <h2 className="text-lg font-semibold p-5 border-b border-b-gray-200">
             To Do List
           </h2>
-          <div className="px-6 py-6">
+          <div className="p-6">
             <FormGroup className="w-full flex flex-col gap-y-1">
               <FormControlLabel
                 control={<Checkbox defaultChecked />}
@@ -65,9 +131,9 @@ const Data = () => {
             </FormGroup>
           </div>
           <div className="text-lg font-semibold p-5 border-t border-t-gray-200 w-full flex justify-end items-center">
-            <IconButton aria-label="fingerprint" className="bg-secondary-600 p-3 text-white hover:bg-secondary-700">
-              <FiPlus />
-            </IconButton>
+            <Fab size="medium" color="primary" aria-label="add">
+              <AddIcon />
+            </Fab>
           </div>
         </div>
         {/* 2 */}
@@ -76,47 +142,62 @@ const Data = () => {
             Traffic Sources
           </h2>
           <div className="px-6 py-6 flex flex-col justify-between items-center gap-y-3">
-          <div className="mb-4 w-full">
+            <div className="mb-4 w-full">
               <div className="w-full flex justify-between items-center mb-2">
                 <p className="text-sm">Direct</p>
                 <p className="text-sm">80%</p>
               </div>
-              <LinearProgress variant="determinate" value={80}/>
+              <LinearProgress variant="determinate" value={80} />
             </div>
             <div className="mb-4 w-full">
               <div className="w-full flex justify-between items-center mb-2">
                 <p className="text-sm">Social</p>
                 <p className="text-sm">50%</p>
               </div>
-              <LinearProgress variant="determinate" value={50} color="inherit" className="bg-primary-200 text-primary-500"/>
+              <LinearProgress
+                variant="determinate"
+                value={50}
+                color="inherit"
+                className="bg-primary-200 text-primary-500"
+              />
             </div>
             <div className="mb-4 w-full">
               <div className="w-full flex justify-between items-center mb-2">
                 <p className="text-sm">Referral</p>
                 <p className="text-sm">20%</p>
               </div>
-              <LinearProgress variant="determinate" value={20}/>
+              <LinearProgress variant="determinate" value={20} />
             </div>
             <div className="mb-4 w-full">
               <div className="w-full flex justify-between items-center mb-2">
                 <p className="text-sm">Bounce</p>
                 <p className="text-sm">60%</p>
               </div>
-              <LinearProgress variant="determinate" value={60} color="inherit" className="bg-primary-200 text-primary-500"/>
+              <LinearProgress
+                variant="determinate"
+                value={60}
+                color="inherit"
+                className="bg-primary-200 text-primary-500"
+              />
             </div>
             <div className="mb-4 w-full">
               <div className="w-full flex justify-between items-center mb-2">
                 <p className="text-sm">Internet</p>
                 <p className="text-sm">40%</p>
               </div>
-              <LinearProgress variant="determinate" value={40}/>
+              <LinearProgress variant="determinate" value={40} />
             </div>
             <div className="mb-4 w-full">
               <div className="w-full flex justify-between items-center mb-2">
                 <p className="text-sm">Social</p>
                 <p className="text-sm">90%</p>
               </div>
-              <LinearProgress variant="determinate" value={90} color="inherit" className="bg-primary-200 text-primary-500"/>
+              <LinearProgress
+                variant="determinate"
+                value={90}
+                color="inherit"
+                className="bg-primary-200 text-primary-500"
+              />
             </div>
           </div>
         </div>
@@ -126,9 +207,9 @@ const Data = () => {
             Team Members
           </h2>
           <div className="px-6 py-6 flex flex-col justify-between items-center gap-y-6">
-          <div className="w-full flex justify-between items-center">
+            <div className="w-full flex justify-between items-center">
               <div className="flex gap-x-2 items-center">
-                <Avatar alt="user" src="/images/avatar-1.png"/>
+                <Avatar alt="user" src="/images/avatar-1.png" />
                 <div className="flex flex-col gap-y-1">
                   <p className="text-sm font-semibold">David Jones</p>
                   <p className="text-xs text-gray-500">Developer</p>
@@ -138,7 +219,7 @@ const Data = () => {
             </div>
             <div className="w-full flex justify-between items-center">
               <div className="flex gap-x-2 items-center">
-                <Avatar alt="user" src="/images/avatar-2.png"/>
+                <Avatar alt="user" src="/images/avatar-2.png" />
                 <div className="flex flex-col gap-y-1">
                   <p className="text-sm font-semibold">David Jones</p>
                   <p className="text-xs text-gray-500">Developer</p>
@@ -148,7 +229,7 @@ const Data = () => {
             </div>
             <div className="w-full flex justify-between items-center">
               <div className="flex gap-x-2 items-center">
-                <Avatar alt="user" src="/images/avatar-3.png"/>
+                <Avatar alt="user" src="/images/avatar-3.png" />
                 <div className="flex flex-col gap-y-1">
                   <p className="text-sm font-semibold">David Jones</p>
                   <p className="text-xs text-gray-500">Developer</p>
@@ -158,7 +239,7 @@ const Data = () => {
             </div>
             <div className="w-full flex justify-between items-center">
               <div className="flex gap-x-2 items-center">
-                <Avatar alt="user" src="/images/avatar-4.png"/>
+                <Avatar alt="user" src="/images/avatar-4.png" />
                 <div className="flex flex-col gap-y-1">
                   <p className="text-sm font-semibold">David Jones</p>
                   <p className="text-xs text-gray-500">Developer</p>
@@ -168,7 +249,7 @@ const Data = () => {
             </div>
             <div className="w-full flex justify-between items-center">
               <div className="flex gap-x-2 items-center">
-                <Avatar alt="user" src="/images/avatar-2.png"/>
+                <Avatar alt="user" src="/images/avatar-2.png" />
                 <div className="flex flex-col gap-y-1">
                   <p className="text-sm font-semibold">David Jones</p>
                   <p className="text-xs text-gray-500">Developer</p>
@@ -178,10 +259,117 @@ const Data = () => {
             </div>
           </div>
           <div className="p-5 border-t border-t-gray-200 w-full flex justify-end">
-            <Button variant="text" size="small" className="capitalize font-bold">View All Projects</Button>
+            <Button
+              variant="text"
+              size="small"
+              className="capitalize font-bold"
+            >
+              View All Projects
+            </Button>
           </div>
-
         </div>
+      </article>
+      {/* 3 */}
+      <article className="grid grid-cols-6 gap-x-6">
+        {/* 1 */}
+        <div className="col-span-3 rounded-md bg-light">
+          <h2 className="text-lg font-semibold p-5 border-b border-b-gray-200">
+            User Activity
+          </h2>
+          <div className="w-full p-6 flex flex-col gap-y-7">
+          <div className="w-full flex justify-between items-center">
+              <div className="flex items-center gap-x-3">
+                <StyledBadgeSuccess
+                  overlap="circular"
+                  anchorOrigin={{ vertical: "bottom", horizontal: "right"}}
+                  variant="dot"
+                >
+                  <Avatar alt="user" src="/images/avatar-1.png" />
+                </StyledBadgeSuccess>
+                <div>
+                  <p className="text-sm font-semibold">John Deo</p>
+                  <p className="text-xs text-gray-500">
+                    Lorem Ipsum is simply dummy text.
+                  </p>
+                </div>
+              </div>
+              <p className="text-xs text-gray-500">
+                <AccessTimeTwoToneIcon className="w-4 h-4" /> 2 min ago
+              </p>
+            </div>
+            <div className="w-full flex justify-between items-center">
+              <div className="flex items-center gap-x-3">
+                <StyledBadgeError
+                  overlap="circular"
+                  anchorOrigin={{ vertical: "bottom", horizontal: "right"}}
+                  variant="dot"
+                >
+                  <Avatar alt="user" src="/images/avatar-2.png" />
+                </StyledBadgeError>
+                <div>
+                  <p className="text-sm font-semibold">John Deo</p>
+                  <p className="text-xs text-gray-500">
+                    Lorem Ipsum is simply dummy text.
+                  </p>
+                </div>
+              </div>
+              <p className="text-xs text-gray-500">
+                <AccessTimeTwoToneIcon className="w-4 h-4" /> 2 min ago
+              </p>
+            </div>
+            <div className="w-full flex justify-between items-center">
+              <div className="flex items-center gap-x-3">
+                <StyledBadgeWarning
+                  overlap="circular"
+                  anchorOrigin={{ vertical: "bottom", horizontal: "right"}}
+                  variant="dot"
+                >
+                  <Avatar alt="user" src="/images/avatar-3.png" />
+                </StyledBadgeWarning>
+                <div>
+                  <p className="text-sm font-semibold">John Deo</p>
+                  <p className="text-xs text-gray-500">
+                    Lorem Ipsum is simply dummy text.
+                  </p>
+                </div>
+              </div>
+              <p className="text-xs text-gray-500">
+                <AccessTimeTwoToneIcon className="w-4 h-4" /> 2 min ago
+              </p>
+            </div>
+            <div className="w-full flex justify-between items-center">
+              <div className="flex items-center gap-x-3">
+                <StyledBadgeSuccess
+                  overlap="circular"
+                  anchorOrigin={{ vertical: "bottom", horizontal: "right"}}
+                  variant="dot"
+                >
+                  <Avatar alt="user" src="/images/avatar-4.png" />
+                </StyledBadgeSuccess>
+                <div>
+                  <p className="text-sm font-semibold">John Deo</p>
+                  <p className="text-xs text-gray-500">
+                    Lorem Ipsum is simply dummy text.
+                  </p>
+                </div>
+              </div>
+              <p className="text-xs text-gray-500">
+                <AccessTimeTwoToneIcon className="w-4 h-4" /> 2 min ago
+              </p>
+            </div>
+          </div>
+          <div className="p-5 border-t border-t-gray-200 w-full flex justify-end">
+            <Button variant="text" size="small" className="capitalize font-semibold">View All Projects</Button>
+          </div>
+        </div>
+        {/* 2 */}
+        <div className="col-span-3 rounded-md bg-light">
+          <h2 className="text-lg font-semibold p-5 border-b border-b-gray-200">
+            Messages
+          </h2>
+          <div ></div>
+        </div>
+
       </article>
     </section>
   );
